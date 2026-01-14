4b3f2595f61fc337c21a860e1e57d6d09d427b92
Nguyen Van Hoai <nguyenvanhoai1020@gmail.com>
2026-01-14 21:18:31 +0700

    🧐 Inspect client codebase

--- apps/client/src/libs/query-client.ts
+++ apps/client/src/libs/query-client.ts
@@ -10,7 +10,7 @@ export const queryClient = new QueryClient({
       refetchOnMount: false,
       refetchOnReconnect: false,
       refetchOnWindowFocus: false,
-      staleTime: 1000 * 60, // 1 minute
+      staleTime: 1000 * 60,
     },
   },
 });
